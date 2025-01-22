7879ff303e4304eba92ec088f876d82b8b03cba8
polygongis <arihtadwiandriani@gmail.com>
2025-01-22 15:00:31 +0700

    Add files via upload

---
+++ styles/jalan_2_style.js
@@ -0,0 +1,329 @@
+var size = 0;
+var placement = 'point';
+function categories_jalan_2(feature, value, size, resolution, labelText,
+                       labelFont, labelFill) {
+                switch(value.toString()) {case 'Ahmad yani':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(123,202,137,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Antasari':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(184,12,218,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'by pass soekarno hatta':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(228,137,219,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'DI Panjaitan':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(201,55,165,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Diponegoro':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(121,219,167,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Endo Suratmin':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(225,77,168,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Gajah Mada':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(71,45,240,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'gg. rumah':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(235,83,157,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'gg.melati':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(88,38,224,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'gg.singkep 1':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(214,114,134,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'H. Komarudin':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(112,221,209,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Imam Bonjol':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(57,227,65,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'jalam perumahan':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(72,135,218,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'jalan kantor':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(125,227,114,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'jalan lokal':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(212,134,55,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'jalan perumahan':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(116,44,217,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'jalan raya natar':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(117,178,204,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'jalan sebiyai':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(120,219,62,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'jl trans sumatera':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(163,96,217,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'jl. Kacapiring':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(63,195,218,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'jl. komplek':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(162,230,35,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'jl. sekolah':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(224,213,89,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Jl. Sutan Jamil':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(78,209,174,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Kapt Abdul Haq':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(107,123,214,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'kapten abdul haq':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(130,130,233,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Karimun Jawa':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(46,230,104,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Kartini':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(119,212,20,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'M. Yamin':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(230,178,87,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'MT Haryono':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(207,114,210,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Nusa Indah':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(116,220,179,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Pramuka':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(162,23,226,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Pulau Legundi':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(217,96,104,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Pulau Linggano':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(232,240,70,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Pulau Singkep':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(209,148,116,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Radin inten':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(72,150,210,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Sento Alibasa':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(213,176,54,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Sudirman':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(62,108,222,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Teuku Umar':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(202,84,73,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Tirtayasa':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(86,237,239,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Woltermongosidi':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(178,214,32,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'Yos Sudarso':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(96,229,55,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+case 'ZA Pagar alam':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(231,33,99,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(227,155,135,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })];
+                    break;}};
+
+var style_jalan_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("Nama jalan");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10.725px \'MS Shell Dlg 2\', sans-serif";
+    var labelFill = "rgba(0, 0, 0, 1)";
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if (feature.get("Nama jalan") !== null) {
+        labelText = String(feature.get("Nama jalan"));
+    }
+    
+var style = categories_jalan_2(feature, value, size, resolution, labelText,
+                          labelFont, labelFill);
+
+    return style;
+};
